52e8fee775df75609cbdeceeb598a19790ed06b7
thegamerhat <thegamerhat@cybrdise.com>
2023-04-15 06:32:01 +0000

    remove rate limiting

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,9 @@
 /**
  * * Trying to use express server just like this template: https://github.com/remotion-dev/template-still
+ * * You can also implement Rate limiting from it, if needed when deploying this project as a site.
  */
 
 import express from 'express';
-// Import rateLimit from 'express-rate-limit';
 import {createTextToSpeechAudio} from './TextToSpeech';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -15,14 +15,8 @@ dotenv.config();
 const app = express();
 const port = process.env.SERVER_PORT || 5050;
 
-// This setting will reveal the real IP address of the user, so we can apply rate limiting.
-// app.set('trust proxy', 1);
-
 app.use(express.json());
 app.use(cors({origin: '*'}));
-
-// Not more than 3 requests per second per user
-// app.use(rateLimit({windowMs: 1 * 1000, max: 3}));
 
 app.post(`/getdata`, async (req, res) => {
 	try {
