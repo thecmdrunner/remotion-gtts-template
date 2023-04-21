5e6c1dfc941842fdf87e4149d137d55def7d56b0
Jonny Burger <jonathanburger11@gmail.com>
2023-04-21 09:28:45 +0200

    fix windows spawning

--- src/preview.ts
+++ src/preview.ts
@@ -4,4 +4,5 @@ import {spawn} from 'child_process';
 startServer();
 spawn('npx', ['remotion', 'preview'], {
 	stdio: 'inherit',
+	shell: process.platform === 'win32' ? 'cmd.exe' : undefined,
 });
--- src/render.ts
+++ src/render.ts
@@ -5,7 +5,10 @@ export const render = async () => {
 	const server = startServer();
 
 	await new Promise<void>((resolve, reject) => {
-		const proc = spawn('npx', ['remotion', 'render'], {stdio: 'inherit'});
+		const proc = spawn('npx', ['remotion', 'render'], {
+			stdio: 'inherit',
+			shell: process.platform === 'win32' ? 'cmd.exe' : undefined,
+		});
 		proc.on('exit', (code) => {
 			if (code === 0) {
 				resolve();
